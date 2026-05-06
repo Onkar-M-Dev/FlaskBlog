2254adba7250854afdfe4ac7ecfeb1e87324df16
Onkar-M-Dev <onkar.marnee1320@gmail.com>
2026-05-06 15:43:34 +0530

    Fix mail configuration

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'devkey')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
+        'SQLALCHEMY_DATABASE_URI',
         'sqlite:///site.db'
     )
 
